54e810b366096ff23b96e8ccbd6ea1d024b2d320
Caleb Hurley <calebhurley@protonmail.com>
2020-05-19 12:12:47 -0800

    Fix WSGI

--- maintenance/settings/main.py
+++ maintenance/settings/main.py
@@ -17,7 +17,7 @@ from .database import *
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "hfaiudgfdsig"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
