656a7dde26815ea6b72a1e064112da77c90c7829
Caleb Hurley <calebhurley@protonmail.com>
2020-05-19 11:40:11 -0800

    Fix Max SQL Limit

--- maintenance/models.py
+++ maintenance/models.py
@@ -414,7 +414,7 @@ class WorkOrder(DefaultMixin):
 
     # WO Details
     location = models.CharField(max_length=500, blank=True, null=True)
-    description = models.CharField(max_length=5000, blank=True, null=True)
+    description = models.CharField(max_length=4000, blank=True, null=True)
 
     def __str__(self):
         return "WO%s" % (self.id,)
